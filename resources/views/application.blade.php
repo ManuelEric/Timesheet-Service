f750098e0db46578e830939c7a1964d9a72d9e97
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2024-08-14 09:00:08 +0700

    ET-68 update layout download

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -35,21 +35,6 @@
             /* warna thumb saat dihover */
         }
     </style>
-
-    <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
-    <script>
-        // Enable pusher logging - don't include this in production
-        Pusher.logToConsole = true;
-
-        var pusher = new Pusher('e69b02b944a4b7ffb31c', {
-            cluster: 'ap1'
-        });
-
-        var channel = pusher.subscribe('my-channel');
-        channel.bind('my-event', function(data) {
-            alert(data.message);
-        });
-    </script>
 </head>
 
 <body>
