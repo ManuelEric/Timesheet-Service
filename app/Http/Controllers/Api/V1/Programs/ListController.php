61f84d150ed5d808a12e719eb6e074352e087a47
unknown <manuel.eric.b@gmail.com>
2024-08-14 15:02:31 +0700

    ET-99 feat: updated list controller

--- app/Http/Controllers/Api/V1/Programs/ListController.php
+++ app/Http/Controllers/Api/V1/Programs/ListController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Ref_Program;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
 
 class ListController extends Controller
 {
