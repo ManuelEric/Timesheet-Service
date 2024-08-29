f9cf8507f760a17354c25273991be517b659801e
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2024-08-29 11:56:48 +0700

    update completed cut_off

--- resources/js/pages/admin/timesheet/timesheet-detail.vue
+++ resources/js/pages/admin/timesheet/timesheet-detail.vue
@@ -1,6 +1,6 @@
 <script setup>
-import UserDetail from '@/components/admin/timesheet/user-detail.vue'
 import TimesheetDetail from '@/components/admin/timesheet/timesheet-detail.vue'
+import UserDetail from '@/components/admin/timesheet/user-detail.vue'
 const props = defineProps({ id: Number })
 const timesheet_id = props.id
 </script>
