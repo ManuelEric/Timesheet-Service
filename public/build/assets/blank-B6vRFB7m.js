fc9d7dcefdd6886c78081f53e790dbbc0c9d39b9
unknown <manuel.eric.b@gmail.com>
2024-09-25 16:27:30 +0700

    build

--- public/build/assets/blank-B6vRFB7m.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as t,b as c,l as n,o as a}from"./main-Bi9r-4Ql.js";const r={},s={class:"layout-wrapper layout-blank"};function _(l,p){const e=n("RouterView");return a(),t("div",s,[c(e)])}const u=o(r,[["render",_]]);export{u as default};
