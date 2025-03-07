4297f6cb5c6468a20f07c6d01325ad16ca161d90
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2025-03-07 11:21:35 +0700

    update curriculum

---
+++ public/build/assets/ssrBoot-BgL4C7Wt.js
@@ -0,0 +1,1 @@
+import{az as e,e as t,K as s,ax as n}from"./main-CzT7uzRK.js";function i(){const o=e(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
